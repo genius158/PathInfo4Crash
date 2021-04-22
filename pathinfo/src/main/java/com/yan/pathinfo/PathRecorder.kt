86944c728b0aa72719dc2d58261fef09e367978e
Bevan <xianwei.yan@quvideo.com>
2021-04-22 19:53:39 +0800

    feat: StringBuilder 清空 采用 delete api

--- pathinfo/src/main/java/com/yan/pathinfo/PathRecorder.kt
+++ pathinfo/src/main/java/com/yan/pathinfo/PathRecorder.kt
@@ -1,15 +1,10 @@
 package com.yan.pathinfo
 
-import java.io.Serializable
-
 /**
  * @author Bevan (Contact me: https://github.com/genius158)
  * @since  2021/3/15
  */
-class PathRecorder : Serializable {
-    companion object {
-        private const val serialVersionUID: Long = 0L
-    }
+class PathRecorder {
 
     val pathInfoList = ArrayList<String>()
 
@@ -26,7 +21,7 @@ class PathRecorder : Serializable {
     override fun toString(): String {
         val pb = pathBuilder
 //        pb.clear()
-        pb.delete(0,pb.length)
+        pb.delete(0, pb.length)
         val iterator = pathInfoList.iterator()
         while (iterator.hasNext()) {
             val path = iterator.next()
